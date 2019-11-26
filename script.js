bc690561f582083d3bed8dd7595e7e89c92770c6
Esteban Rodriguez <erodriguez0@users.noreply.github.com>
2019-11-26 13:58:08 -0800

    navbar collapse on click (mobile)

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 	
+	$(function(){ 
+	     var navMain = $(".navbar-collapse"); // avoid dependency on #id
+	     // "a:not([data-toggle])" - to avoid issues caused
+	     // when you have dropdown inside navbar
+	     navMain.on("click", "a:not([data-toggle])", null, function () {
+	         navMain.collapse('hide');
+	     });
+	 });
+	
 	// $('body').scrollspy({
 	// 	target: '#navbar',
 	// 	offset: 56
