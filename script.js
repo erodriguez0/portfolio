213b2581edd5c0a435cb5eded4a965c69e984a18
Esteban Rodriguez <erodriguez0@users.noreply.github.com>
2019-12-15 20:58:58 -0800

    projects page

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
+	function scrollToDiv(id) {
+		$('html, body').animate({scrollTop: $("#"+id).offset().top});
+	}
 
+	$("#projects-btn").click(function() {
+		scrollToDiv("projects");
+	});
 });
