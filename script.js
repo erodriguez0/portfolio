75fe3431b38a85428a74dad3ec38ce32cf74b01b
Esteban Rodriguez <erodriguez0705@gmail.com>
2020-09-02 17:07:01 -0700

    Portfolio redesign

--- script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	function scrollToDiv(id) {
-		$('html, body').animate({scrollTop: $("#"+id).offset().top});
-	}
-
-	$("#projects-btn").click(function() {
-		scrollToDiv("projects");
-	});
-});
